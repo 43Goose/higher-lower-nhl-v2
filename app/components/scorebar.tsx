d4e53d1a17eeec113d7ae90589baf08b6a2c8eff
Owen Edwards <owen.allen.edwards@gmail.com>
2024-07-30 16:38:05 -0700

    hotfix 1 - game buttons covered by scorebar

--- app/components/scorebar.tsx
+++ app/components/scorebar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Scorebar({ score, highscore }: { score: number; highscore?: number | undefined }) {
     return (
-        <div className='absolute top-0 w-full flex justify-between px-8 py-4 md:bottom-0'>
+        <div className='absolute top-0 h-min w-full flex justify-between px-8 py-4 md:bottom-0'>
             <h1 className='text-xl font-bold'>{'Score: ' + score}</h1>
             <h1 className='text-xl font-bold'>{highscore !== undefined ? 'Highscore: ' + highscore : ''}</h1>
         </div>
